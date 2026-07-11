9ae49796590e2d0705cba700e050588c1caf7325
sefq366-ship-it <sefq366@gmail.com>
2026-07-11 22:42:46 +0300

    Update themes.js

--- js/themes.js
+++ js/themes.js
@@ -1,31 +1,22 @@
+// js/themes.js
 class ThemeManager {
     constructor() {
         this.currentTheme = localStorage.getItem('gram2profit_theme') || 'midnight';
         this.applyTheme(this.currentTheme);
     }
-    
     applyTheme(themeName) {
         document.body.className = 'theme-' + themeName;
         localStorage.setItem('gram2profit_theme', themeName);
         this.currentTheme = themeName;
-        
         document.querySelectorAll('.theme-option').forEach(function(opt) {
-            if (opt.dataset.theme === themeName) {
-                opt.classList.add('active');
-            } else {
-                opt.classList.remove('active');
-            }
+            opt.classList.toggle('active', opt.dataset.theme === themeName);
         });
     }
-    
     init() {
         var self = this;
         document.querySelectorAll('.theme-option').forEach(function(option) {
-            option.addEventListener('click', function() {
-                self.applyTheme(this.dataset.theme);
-            });
+            option.addEventListener('click', function() { self.applyTheme(this.dataset.theme); });
         });
     }
 }
-
 var themeManager = new ThemeManager();
